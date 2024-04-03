6b30e63dd7b31a3d37cc480a94f781fabb6afb9d
Shalom Ogunshola <shalomogunshola@gmail.com>
2024-04-03 21:45:57 +0100

    update patient entity

--- src/modules/patients/entities/patient.entity.ts
+++ src/modules/patients/entities/patient.entity.ts
@@ -1,42 +1,46 @@
-import { Column, Entity } from "typeorm";
-import { AbstractEntity } from "../../../shared/entities/abstract-entity";
-import { EnrollmentStatus, IPatient } from "../interfaces/patient.interface";
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { AbstractEntity } from '../../../shared/entities/abstract-entity';
+import { EnrollmentStatus, IPatient } from '../interfaces/patient.interface';
+import { Company } from '../../companies/entities/company.entity';
 
 @Entity('patients')
 export class Patient extends AbstractEntity implements IPatient {
-    @Column()
-    patientId: string;
+  @Column()
+  patientId: string;
 
-    @Column({type: 'timestamp'})
-    enrollmentDate: string;
+  @Column({ type: 'timestamp' })
+  enrollmentDate: string;
 
-    @Column({type: 'enum', enum: EnrollmentStatus})
-    enrollmentStatus: EnrollmentStatus;
+  @Column({ type: 'enum', enum: EnrollmentStatus })
+  enrollmentStatus: EnrollmentStatus;
 
-    @Column()
-    birthDate: string;
+  @Column()
+  birthDate: string;
 
-    @Column()
-    gender: 'male' | 'female';
+  @Column()
+  gender: 'male' | 'female';
 
-    @Column()
-    nationality: string;
+  @Column()
+  nationality: string;
 
-    @Column()
-    consentReceived: boolean;
+  @Column()
+  consentReceived: boolean;
 
-    @Column()
-    commencedTreatment: boolean;
+  @Column()
+  commencedTreatment: boolean;
 
-    @Column()
-    droppedOut: boolean;
+  @Column()
+  droppedOut: boolean;
 
-    @Column()
-    visitCompleted: boolean;
+  @Column()
+  visitCompleted: boolean;
 
-    @Column()
-    withinWindow: boolean;
+  @Column()
+  withinWindow: boolean;
 
-    @Column()
-    companyId: string;
+  @Column()
+  companyId: string;
+
+  @ManyToOne(() => Company, { eager: true })
+  company: Company;
 }
